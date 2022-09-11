f23de4d22e56d2f6c1f7de30656921dd0d379d5b
Artem <artem2015-asd@yandex.ru>
2022-09-11 11:07:46 +0300

    HW done

---
+++ EX4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+string squareTable(int num)
+{
+    string result = $"{num} -> 1";
+    for (int i = 2; i <= num; i++)
+    {
+        result = result + $", {Math.Pow(i,3)}";
+    }
+    return result;
+}
+
+try
+{
+    Console.WriteLine("Введите число = ");
+    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine(squareTable(num));
+}
+catch
+{
+    Console.WriteLine("Вводите целочисленные значения");
+}
